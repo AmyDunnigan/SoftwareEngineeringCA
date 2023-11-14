60b947b5766a1e59e2b7d9ddc0555473b29a4ea0
AmyDunnigan <amyldunnigan@gmail.com>
2023-11-14 21:35:22 +0000

    Testing multiple controls and objects

--- main2.cpp
+++ main2.cpp
@@ -8,6 +8,9 @@ int main() {
     // Initialize the Window
     InitWindow(screenWidth, screenHeight, "Input Movement Example");
 
+    // Box position and control from Raylib exambles with screen position adjusted
+    int boxPositionY = screenHeight/3 - 40;
+    int scrollSpeed = 4;  
  
     Texture2D myTexture = LoadTexture("Resources/Textures/spider.png");
     Vector2 scale = {0.5f,0.5f};
@@ -30,6 +33,11 @@ int main() {
     while (!WindowShouldClose() /*WindowShouldClose returns true if esc is clicked and closes the window*/) {
 
     {
+
+    // Box controls from Raylib example
+        boxPositionY -= (GetMouseWheelMove()*scrollSpeed);
+        
+    // Spider controls from class example
         if(IsKeyDown(KEY_RIGHT)) position.x += moveSpeed;
         if(IsKeyDown(KEY_LEFT)) position.x -= moveSpeed;
         if(IsKeyDown(KEY_UP)) position.y -= moveSpeed;
@@ -41,6 +49,12 @@ int main() {
         // Setup Canvas
         BeginDrawing();
         // Clear canvas to a specific color to avoid flicker
+        DrawRectangle(screenWidth/2 - 40, boxPositionY, 80, 80, MAROON);
+
+        DrawText("Use mouse wheel to move the cube up and down!", 10, 10, 20, GRAY);
+        DrawText(TextFormat("Box position Y: %03i", boxPositionY), 10, 40, 20, LIGHTGRAY);
+        
+        
         ClearBackground(RAYWHITE);
         DrawTextureEx(myTexture, position, rotation, 0.5f, tint);
 
