ff1fd53bebe269f1ca67dc9b0b2f9ede2096d1bd
AmyDunnigan <amyldunnigan@gmail.com>
2023-11-23 16:48:59 +0000

    More random experiments

--- main2.cpp
+++ main2.cpp
@@ -1,63 +1,65 @@
 #include "raylib.h"
+#include "raymath.h"
 
-int main() {
-    // Determine the Game Window Width and Height
+// Defines modified from Pang game
+# define RandomSpider 
+# define SpiderSpeed    2.0f
+
+// Types and Structures definition modified from Pang game
+typedef struct RandomSpider {
+    Vector2 position;
+    Vector2 speed;
+    float radius;
+    int points;
+    bool active;
+    } RandomSpider;
+
+typedef struct Points {
+    Vector2 position;
+    int value;
+    float alpha;
+    } Points;
+
+    // Global variables declaration
+
+int main(void) {
+    
     const int screenWidth = 800;
     const int screenHeight = 800;
 
     // Initialize the Window
     InitWindow(screenWidth, screenHeight, "Input Movement Example");
+    
+    // Setting the Frames Per Second
+    SetTargetFPS(60);
 
     // Box position and control from Raylib exambles with screen position adjusted
     int boxPositionY = screenHeight/3 - 40;
     int scrollSpeed = 4;
 
-    // Initialising position for random object
+    // Initialise modified Pang game variables
     int posx, posy;
-    int velx, vely;  
-    bool correctRange = false;
+    int velx = 0;
+    int vely = 0;
 
-    // Initialise random object copied from asteroid game
-        for (int i = 0; i < 2; i++)
+    // Initialising random object modified from pang game
+    for (int i = 0; i < RandomSpider; i++)
     {
-        posx = GetRandomValue(0, screenWidth);
+        RandomSpider[i].radius = 40.0f;
+        posx = GetRandomValue(0 + RandomSpider[i].radius, screenWidth - RandomSpider[i].radius);
+        posy = GetRandomValue(0 + RandomSpider[i].radius, screenHeight/2);
 
-        while (!correctRange)
+        RandomSpider[i].position = (Vector2){ posx, posy };
+
+        while ((velx == 0) || (vely == 0))
         {
-            if (posx > screenWidth/2 - 150 && posx < screenWidth/2 + 150) posx = GetRandomValue(0, screenWidth);
-            else correctRange = true;
+            velx = GetRandomValue(-SpiderSpeed, SpiderSpeed);
+            vely = GetRandomValue(-SpiderSpeed, SpiderSpeed);
         }
 
-        correctRange = false;
-
-        posy = GetRandomValue(0, screenHeight);
-
-        while (!correctRange)
-        {
-            if (posy > screenHeight/2 - 150 && posy < screenHeight/2 + 150)  posy = GetRandomValue(0, screenHeight);
-            else correctRange = true;
-        }
-
-        [i].position = (Vector2){posx, posy};
-
-        correctRange = false;
-        velx = GetRandomValue(0, 20);
-        vely = GetRandomValue(0, 20);
-
-        while (!correctRange)
-        {
-            if (velx == 0 && vely == 0)
-            {
-                velx = GetRandomValue(0, 20);
-                vely = GetRandomValue(0, 20);
-            }
-            else correctRange = true;
-        }
-
-        [i].speed = (Vector2){velx, vely};
-        [i].radius = 40;
-        [i].active = true;
-        [i].color = BLUE;
+        RandomSpider[i].speed = (Vector2){ velx, vely };
+        RandomSpider[i].points = 200;
+        RandomSpider[i].active = true;
     }
       
     // Define spider
@@ -65,15 +67,9 @@ int main() {
     Vector2 scale = {0.5f,0.5f};
     float rotation = 0.0f;
     Color tint = WHITE;
-    Vector2 position = {(screenWidth -(myTexture.width *scale.x))/2, (screenHeight-(myTexture.height*scale.y))/2};
+    Vector2 position = {(screenWidth -(myTexture.width*scale.x))/2, (screenHeight-(myTexture.height*scale.y))/2};
     const float moveSpeed = 5.0f;
-
-    
-    
-
-    // Setting the Frames Per Second
-    SetTargetFPS(60);
-
+     
     // Setting motion and rotation
 
     // The Game Loop
@@ -91,6 +87,7 @@ int main() {
         if(IsKeyDown(KEY_DOWN)) position.y += moveSpeed;
         if(IsMouseButtonPressed(MOUSE_LEFT_BUTTON)) rotation -= 10.0f;
         if(IsMouseButtonPressed(MOUSE_RIGHT_BUTTON)) rotation += 10.0f;
+
     }
 
 
@@ -106,6 +103,14 @@ int main() {
 
         DrawText("Use mouse wheel to move the cube up and down!", 10, 10, 20, GRAY);
         DrawText(TextFormat("Box position Y: %03i", boxPositionY), 10, 40, 20, LIGHTGRAY);
+
+        // Draw RandomSpider modified from Pang game
+
+            for (int i = 0;i < RandomSpider; i++)
+            {
+                if (RandomSpider[i].active) DrawCircleV(RandomSpider[i].position, RandomSpider[i].radius, DARKGRAY);
+                else DrawCircleV(RandomSpider[i].position, RandomSpider[i].radius, Fade(LIGHTGRAY, 0.3f));
+            }
         
         
         ClearBackground(RAYWHITE);
