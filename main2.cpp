937971f63bbaa4275b22799e9306b2fd6674e88a
AmyDunnigan <amyldunnigan@gmail.com>
2023-11-25 17:40:30 +0000

    Multiple textures

--- main2.cpp
+++ main2.cpp
@@ -8,15 +8,21 @@ int main() {
     // Initialize the Window
     InitWindow(screenWidth, screenHeight, "Input Movement Example");
 
- 
+    // Draw background    
+    Texture2D deskTexture = LoadTexture("Resources/Textures/desk.png");
+
+    // Initialize cup 
     Texture2D myTexture = LoadTexture("Resources/Textures/cup.png");
     Vector2 scale = {0.5f,0.5f};
-    float rotation = 0.0f;
     Color tint = WHITE;
     Vector2 position = {(screenWidth -(myTexture.width *scale.x))/2, (screenHeight-(myTexture.height*scale.y))/2};
-
     const float moveSpeed = 5.0f;
 
+    // Initialize pen
+    Texture2D penTexture = LoadTexture("Resources/Textures/pen.png");
+    float rotation = 0.0f;
+   
+    // Initialise box from Raylib example
     int boxPositionY = screenHeight/2 - 20;
     int scrollSpeed = 4;
 
@@ -27,30 +33,35 @@ int main() {
 
     // The Game Loop
     while (!WindowShouldClose() /*WindowShouldClose returns true if esc is clicked and closes the window*/) {
-
+    
+    // Set cup movement
     {
         if(IsKeyDown(KEY_RIGHT)) position.x += moveSpeed;
         if(IsKeyDown(KEY_LEFT)) position.x -= moveSpeed;
         if(IsKeyDown(KEY_UP)) position.y -= moveSpeed;
         if(IsKeyDown(KEY_DOWN)) position.y += moveSpeed;
+
+    }
+
+    // Set pen movement
+    {
         if(IsMouseButtonPressed(MOUSE_LEFT_BUTTON)) rotation -= 10.0f;
         if(IsMouseButtonPressed(MOUSE_RIGHT_BUTTON)) rotation += 10.0f;
     }
-
     // Add box from Raylib examples
     {
     boxPositionY -= (GetMouseWheelMove()*scrollSpeed);
-
     }
 
         // Setup Canvas
         BeginDrawing();
         // Clear canvas to a specific color to avoid flicker
         ClearBackground(RAYWHITE);
+        DrawTexture(deskTexture,0,0,WHITE);
         DrawTextureEx(myTexture, position, rotation, 0.5f, tint);
         DrawRectangle(screenWidth/3 - 40, boxPositionY, 80, 80, MAROON);
         DrawText("Use mouse wheel to move the cube up and down!", 10, 10, 20, GRAY);
-        
+        DrawTextureEx(penTexture, position, rotation, 0.5f, tint);
 
         // Here goes all the Game Logic
 
@@ -58,6 +69,7 @@ int main() {
         EndDrawing();
     }
     UnloadTexture(myTexture);
+    UnloadTexture(penTexture);
     CloseWindow();
     return 0;
 }
