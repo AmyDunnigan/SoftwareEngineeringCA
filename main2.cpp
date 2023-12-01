c8bde34eb6a0f74ce78cc549c283aa0baaf9ae7b
AmyDunnigan <amyldunnigan@gmail.com>
2023-12-01 15:50:00 +0000

    Test game with asteroids

--- main2.cpp
+++ main2.cpp
@@ -1,210 +1,358 @@
-
-#include <stdlib.h> // includes functions involving memory, process control and conversions
-#include <time.h> // includes functions for manipulating time
+/*******************************************************************************************
+*
+*   raylib - classic game: asteroids survival
+*
+*   Sample game developed by Ian Eito, Albert Martos and Ramon Santamaria
+*
+*   This game has been created using raylib v1.3 (www.raylib.com)
+*   raylib is licensed under an unmodified zlib/libpng license (View raylib.h for details)
+*
+*   Copyright (c) 2015 Ramon Santamaria (@raysan5)
+*
+********************************************************************************************/
 
 #include "raylib.h"
-#include "raymath.h"
+#include <math.h>
 
-// Defining pre-processor constants
-#define SNOW_SOURCE_WIDTH 240
-#define SNOW_SOURCE_HEIGHT 240
-#define SNOW_MAX_COUNT 16
-#define SNOW_SOURCE_RECTANGLE CLITERAL(Rectangle){0, 0, SNOW_SOURCE_WIDTH, SNOW_SOURCE_HEIGHT}
-#define FALL_SPEED_MIN 10
-#define FALL_SPEED_MAX 50
+#if defined(PLATFORM_WEB)
+    #include <emscripten/emscripten.h>
+#endif
 
+//----------------------------------------------------------------------------------
+// Some Defines
+//----------------------------------------------------------------------------------
+#define PLAYER_BASE_SIZE    20.0f
+#define PLAYER_SPEED        6.0f
+#define PLAYER_MAX_SHOOTS   10
 
-// Determine the Game Window Width and Height
-const int screenWidth = 800;
-const int screenHeight = 800;
-const Vector2 screenSize = {screenWidth, screenHeight};
+#define METEORS_SPEED       2
+#define MAX_MEDIUM_METEORS  8
+#define MAX_SMALL_METEORS   16
 
-// Create struct for snow
-typedef struct Snow
-{
+#define MAX_FRAME_SPEED     15
+#define MIN_FRAME_SPEED      1
+
+//----------------------------------------------------------------------------------
+// Types and Structures Definition
+//----------------------------------------------------------------------------------
+typedef struct Player {
     Vector2 position;
-    int fallSpeed;
-    bool _active;
-}Snow;
+    float speed;
+    float acceleration;
+    float rotation;
+    Vector3 collider;
+    Color color;
+    Texture2D texture;
+    Rectangle sourceRec;
+} Player;
 
-// Snow array
-Snow _snow[SNOW_MAX_COUNT];
+typedef struct Meteor {
+    Vector2 position;
+    Vector2 speed;
+    float radius;
+    bool active;
+    Color color;
+} Meteor;
 
-typedef enum GameState
+//------------------------------------------------------------------------------------
+// Global Variables Declaration
+//------------------------------------------------------------------------------------
+static const int screenWidth = 800;
+static const int screenHeight = 450;
 
-{PLAYING,
-END
-}GameState;
+static bool gameOver = false;
+static bool pause = false;
+static Player player = { 0 };
+int currentFrame = 0;
 
-GameState _state; // Denotes state of game - Global variables prefixed with an underscore
+int framesCounter = 0;
+int framesSpeed = 8; 
 
-float _timeGameStarted;
-float _timeGameEnded;
+static Meteor mediumMeteor[MAX_MEDIUM_METEORS] = { 0 };
+static Meteor smallMeteor[MAX_SMALL_METEORS] = { 0 };
 
-Texture2D _spriteSnow;
-Texture2D _spriteScarfy;
+//------------------------------------------------------------------------------------
+// Module Functions Declaration (local)
+//------------------------------------------------------------------------------------
+static void InitGame(void);         // Initialize game
+static void UpdateGame(void);       // Update game (one frame)
+static void DrawGame(void);         // Draw game (one frame)
+static void UnloadGame(void);       // Unload game
+static void UpdateDrawFrame(void);  // Update and Draw (one frame)
 
-void UnsetSnowAt(int);
-void SetSnowAt(int, Vector2, float);
-void DrawSnow(void);
-void UpdateSnow(void);
 
-void GameInit(void);
-void GameEnd(void);
-void UpdateDrawFrame(void);
 
-int main() {
-    // Initialize the Window
-    srand(time(0));
+//------------------------------------------------------------------------------------
+// Program main entry point
+//------------------------------------------------------------------------------------
+int main(void)
+{
+    // Initialization (Note windowTitle is unused on Android)
+    //---------------------------------------------------------
+    InitWindow(screenWidth, screenHeight, "classic game: asteroids survival");
     
-    InitWindow(screenWidth, screenHeight, "Basic game modified from example by Andrew Hamel");
-    
-    // Load textures
-    _spriteSnow = LoadTexture("Resources/Textures/cup.png");
-    _spriteScarfy = LoadTexture("Resources/Textures/scarfy.png");
+    struct Player player;
+    player.position = (Vector2){screenWidth/2, screenHeight/2};
+    player.speed = 5.0f;
+    player.texture = LoadTexture("Resources/Textures/scarfy.png");
+    player.sourceRec = (Rectangle){0.0f, 0.0f, (float)(player.texture.width)/6, (float)(player.texture.height)};
+  
+    InitGame();
 
-    GameInit();
+#if defined(PLATFORM_WEB)
+    emscripten_set_main_loop(UpdateDrawFrame, 60, 1);
+#else
 
-    // Box position and control from Raylib exambles with screen position adjusted
+    SetTargetFPS(60);
+    //--------------------------------------------------------------------------------------
 
-    // Setting motion and rotation
+    // Main game loop
+    while (!WindowShouldClose())    // Detect window close button or ESC key
+    {
 
-    // The Game Loop here goes all the Game Logic
-    while (!WindowShouldClose() /*WindowShouldClose returns true if esc is clicked and closes the window*/) 
-
-        // Setup Canvas
-        {
+        if (IsKeyDown(KEY_RIGHT)) player.position.x += player.speed;
+        if (IsKeyDown(KEY_LEFT)) player.position.x -= player.speed;
+        if (IsKeyDown(KEY_UP)) player.position.x -= player.speed;
+        if (IsKeyDown(KEY_DOWN)) player.position.x += player.speed;
+        // Update and Draw
+        //----------------------------------------------------------------------------------
         UpdateDrawFrame();
-        }
+        //----------------------------------------------------------------------------------
+    }
+#endif
+    // De-Initialization
+    //--------------------------------------------------------------------------------------
+    UnloadGame();         // Unload loaded data (textures, sounds, models...)
 
-        CloseWindow();
+    CloseWindow();        // Close window and OpenGL context
+    //--------------------------------------------------------------------------------------
 
     return 0;
 }
 
-void UnsetSnowAt(int i)
+//------------------------------------------------------------------------------------
+// Module Functions Definitions (local)
+//------------------------------------------------------------------------------------
+
+// Initialize game variables
+void InitGame(void)
 {
-    if (i < 0 || i >= SNOW_MAX_COUNT) // Early Out - Ensure variables are within parameters to avoid errors
+    int posx, posy;
+    int velx, vely;
+    bool correctRange = false;
+
+    pause = false;
+
+    // Initialization player
+    player.position = (Vector2){screenWidth/2, screenHeight/2};
+    player.acceleration = 0;
+    player.rotation = 0;
+    player.collider = (Vector3){player.position.x + sin(player.rotation*DEG2RAD)*(player.texture.height/2.5f), player.position.y - cos(player.rotation*DEG2RAD)*(player.texture.height/2.5f), 12};
+
+    for (int i = 0; i < MAX_MEDIUM_METEORS; i++)
     {
-        return;
-    }
+        posx = GetRandomValue(0, screenWidth);
 
-    _snow[i]._active = false;
-}
-
-void SetSnowAt(int i, Vector2 position, float fallSpeed)
-{
-    if (i < 0 || i >= SNOW_MAX_COUNT) // Early Out - ensure variables are within parameters to avoid errors
-    {
-        return;
-    }
-
-    _snow[i].position = position;
-    _snow[i].fallSpeed = fallSpeed;
-    _snow[i]._active = true;
-}
-
-void GameInit(void)
-{
-_state = PLAYING;
-_timeGameStarted = GetTime();
-
-for(int i = 0; i < SNOW_MAX_COUNT; i++)
-{
-    UnsetSnowAt(i);
-}
-}
-
-void DrawSnow(void)
-{
-for(int i = 0; i < SNOW_MAX_COUNT; i++)
-{
-    if(!_snow[i]._active)
-    {
-        continue;
-    }
-    // Set drawing to centre of sprite
-    Vector2 position = _snow[i].position;
-    position.x -= SNOW_SOURCE_WIDTH /2;
-    position.y -= SNOW_SOURCE_HEIGHT /2;
-    DrawTextureRec(_spriteSnow, SNOW_SOURCE_RECTANGLE, _snow[i].position, WHITE);
-}  
-}
-
-void UpdateSnow(void)
-{
-for(int i = 0; i < SNOW_MAX_COUNT; i++)
-{
-    if(!_snow[i]._active)
-    {
-        continue;
-    }
-
-    float offset = _snow[i].fallSpeed * GetFrameTime(); // for consistent play speed
-    _snow[i].position.y += offset;
-}  
-}
-
-void SpawnSnow (void)
-{
-    int availableIndex = -1;
-    for (int i = 0; i < SNOW_MAX_COUNT; i++)
-    {
-        if (_snow[i]._active)
+        while (!correctRange)
         {
-            availableIndex = i;
-            break;
+            if (posx > screenWidth/2 - 150 && posx < screenWidth/2 + 150) posx = GetRandomValue(0, screenWidth);
+            else correctRange = true;
+        }
+
+        correctRange = false;
+
+        posy = GetRandomValue(0, screenHeight);
+
+        while (!correctRange)
+        {
+            if (posy > screenHeight/2 - 150 && posy < screenHeight/2 + 150)  posy = GetRandomValue(0, screenHeight);
+            else correctRange = true;
+        }
+
+        correctRange = false;
+        velx = GetRandomValue(-METEORS_SPEED, METEORS_SPEED);
+        vely = GetRandomValue(-METEORS_SPEED, METEORS_SPEED);
+
+        while (!correctRange)
+        {
+            if (velx == 0 && vely == 0)
+            {
+                velx = GetRandomValue(-METEORS_SPEED, METEORS_SPEED);
+                vely = GetRandomValue(-METEORS_SPEED, METEORS_SPEED);
+            }
+            else correctRange = true;
+        }
+        mediumMeteor[i].position = (Vector2){posx, posy};
+        mediumMeteor[i].speed = (Vector2){velx, vely};
+        mediumMeteor[i].radius = 20;
+        mediumMeteor[i].active = true;
+        mediumMeteor[i].color = GREEN;
+    }
+
+    for (int i = 0; i < MAX_SMALL_METEORS; i++)
+    {
+        posx = GetRandomValue(0, screenWidth);
+
+        while (!correctRange)
+        {
+            if (posx > screenWidth/2 - 150 && posx < screenWidth/2 + 150) posx = GetRandomValue(0, screenWidth);
+            else correctRange = true;
+        }
+
+        correctRange = false;
+
+        posy = GetRandomValue(0, screenHeight);
+
+        while (!correctRange)
+        {
+            if (posy > screenHeight/2 - 150 && posy < screenHeight/2 + 150)  posy = GetRandomValue(0, screenHeight);
+            else correctRange = true;
+        }
+
+        correctRange = false;
+        velx = GetRandomValue(-METEORS_SPEED, METEORS_SPEED);
+        vely = GetRandomValue(-METEORS_SPEED, METEORS_SPEED);
+
+        while (!correctRange)
+        {
+            if (velx == 0 && vely == 0)
+            {
+                velx = GetRandomValue(-METEORS_SPEED, METEORS_SPEED);
+                vely = GetRandomValue(-METEORS_SPEED, METEORS_SPEED);
+            }
+            else correctRange = true;
+        }
+        smallMeteor[i].position = (Vector2){posx, posy};
+        smallMeteor[i].speed = (Vector2){velx, vely};
+        smallMeteor[i].radius = 10;
+        smallMeteor[i].active = true;
+        smallMeteor[i].color = YELLOW;
+    }
+}
+
+// Update game (one frame)
+void UpdateGame(void)
+{
+    if (!gameOver)
+    {
+        if (IsKeyPressed('P')) pause = !pause;
+
+        if (!pause)
+        {
+            framesCounter++;
+
+            // Player logic
+                   
+            if (framesSpeed > MAX_FRAME_SPEED) framesSpeed = MAX_FRAME_SPEED;
+            else if (framesSpeed < MIN_FRAME_SPEED) framesSpeed = MIN_FRAME_SPEED;
+
+            // Collision Player to meteors
+            player.collider = (Vector3){player.position.x + sin(player.rotation*DEG2RAD)*(player.texture.height/2.5f), player.position.y - cos(player.rotation*DEG2RAD)*(player.texture.height/2.5f), 12};
+
+            for (int a = 0; a < MAX_MEDIUM_METEORS; a++)
+            {
+                if (CheckCollisionCircles((Vector2){player.collider.x, player.collider.y}, player.collider.z, mediumMeteor[a].position, mediumMeteor[a].radius) && mediumMeteor[a].active) gameOver = true;
+            }
+
+            for (int a = 0; a < MAX_SMALL_METEORS; a++)
+            {
+                if (CheckCollisionCircles((Vector2){player.collider.x, player.collider.y}, player.collider.z, smallMeteor[a].position, smallMeteor[a].radius) && smallMeteor[a].active) gameOver = true;
+            }
+
+            // Meteor logic
+
+            for (int i = 0; i < MAX_MEDIUM_METEORS; i++)
+            {
+                if (mediumMeteor[i].active)
+                {
+                    // movement
+                    mediumMeteor[i].position.x += mediumMeteor[i].speed.x;
+                    mediumMeteor[i].position.y += mediumMeteor[i].speed.y;
+
+                    // wall behaviour
+                    if  (mediumMeteor[i].position.x > screenWidth + mediumMeteor[i].radius) mediumMeteor[i].position.x = -(mediumMeteor[i].radius);
+                    else if (mediumMeteor[i].position.x < 0 - mediumMeteor[i].radius) mediumMeteor[i].position.x = screenWidth + mediumMeteor[i].radius;
+                    if (mediumMeteor[i].position.y > screenHeight + mediumMeteor[i].radius) mediumMeteor[i].position.y = -(mediumMeteor[i].radius);
+                    else if (mediumMeteor[i].position.y < 0 - mediumMeteor[i].radius) mediumMeteor[i].position.y = screenHeight + mediumMeteor[i].radius;
+                }
+            }
+
+            for (int i = 0; i < MAX_SMALL_METEORS; i++)
+            {
+                if (smallMeteor[i].active)
+                {
+                    // movement
+                    smallMeteor[i].position.x += smallMeteor[i].speed.x;
+                    smallMeteor[i].position.y += smallMeteor[i].speed.y;
+
+                    // wall behaviour
+                    if  (smallMeteor[i].position.x > screenWidth + smallMeteor[i].radius) smallMeteor[i].position.x = -(smallMeteor[i].radius);
+                    else if (smallMeteor[i].position.x < 0 - smallMeteor[i].radius) smallMeteor[i].position.x = screenWidth + smallMeteor[i].radius;
+                    if (smallMeteor[i].position.y > screenHeight + smallMeteor[i].radius) smallMeteor[i].position.y = -(smallMeteor[i].radius);
+                    else if (smallMeteor[i].position.y < 0 - smallMeteor[i].radius) smallMeteor[i].position.y = screenHeight + smallMeteor[i].radius;
+                }
+            }
         }
     }
-
-    if (availableIndex == -1)
+    else
     {
-        // Checking available index not still -1 as this would be an ERROR!
-        return;
+        if (IsKeyPressed(KEY_ENTER))
+        {
+            InitGame();
+            gameOver = false;
+        }
     }
-
-    int fallspeed = GetRandomValue(FALL_SPEED_MIN,FALL_SPEED_MAX);
-    int posX = GetRandomValue(SNOW_SOURCE_WIDTH / 2, screenWidth - (SNOW_SOURCE_WIDTH / 2));
-
-    SetSnowAt(availableIndex, (Vector2){posX, -SNOW_SOURCE_HEIGHT}, fallspeed);
 }
 
-void GameEnd(void)
+// Draw game (one frame)
+void DrawGame(void)
 {
-_state = END;
-_timeGameEnded = GetTime();
+    BeginDrawing();
+
+        ClearBackground(LIGHTGRAY);
+        
+        DrawTextureRec(player.texture, player.sourceRec, player.position, WHITE);   
+
+        if (!gameOver)
+        {
+            // Draw player
+        
+        DrawTextureRec(player.texture, player.sourceRec, player.position, WHITE);   
+           
+            // Draw meteor
+            for (int i = 0;i < MAX_MEDIUM_METEORS; i++)
+            {
+                if (mediumMeteor[i].active) DrawCircleV(mediumMeteor[i].position, mediumMeteor[i].radius, GRAY);
+                else DrawCircleV(mediumMeteor[i].position, mediumMeteor[i].radius, Fade(LIGHTGRAY, 0.3f));
+            }
+
+            for (int i = 0;i < MAX_SMALL_METEORS; i++)
+            {
+                if (smallMeteor[i].active) DrawCircleV(smallMeteor[i].position, smallMeteor[i].radius, DARKGRAY);
+                else DrawCircleV(smallMeteor[i].position, smallMeteor[i].radius, Fade(LIGHTGRAY, 0.3f));
+            }
+            
+            DrawTextureRec(player.texture, player.sourceRec, player.position, WHITE);   
+            DrawText(TextFormat("TIME: %.02f", (float)framesCounter/60), 10, 10, 20, BLACK);
+
+            if (pause) DrawText("GAME PAUSED", screenWidth/2 - MeasureText("GAME PAUSED", 40)/2, screenHeight/2 - 40, 40, GRAY);
+        }
+        else DrawText("PRESS [ENTER] TO PLAY AGAIN", GetScreenWidth()/2 - MeasureText("PRESS [ENTER] TO PLAY AGAIN", 20)/2, GetScreenHeight()/2 - 50, 20, GRAY);
+
+    EndDrawing();
+    //----------------------------------------------------------------------------------
 }
 
+// Unload game variables
+void UnloadGame(void)
+{
+    // TODO: Unload all dynamic loaded data (textures, sounds, models...)
+}
+
+// Update and Draw (one frame)
 void UpdateDrawFrame(void)
 {
-// reset game
-if (_state == END && IsKeyPressed(KEY_RIGHT))
-{
-    GameInit();    
-}
-
-if (IsMouseButtonPressed(MOUSE_LEFT_BUTTON))
-{
-    SpawnSnow();
-}
-
-UpdateSnow();
-
-BeginDrawing();
-
-ClearBackground(RAYWHITE);
-
-// Draw game
-DrawSnow();
-
-if (_state == END)
-{
-    // TODO: Gameover HUD
-}
-else
-{
-    // TODO: Gametime HUD
-}
-
-
+    UnloadTexture(player.texture);
+    UpdateGame();
+    DrawGame();
 }
