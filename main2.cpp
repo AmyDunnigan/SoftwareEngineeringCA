77c3f6f81e32d5fbda5d75de59816d974b701b27
AmyDunnigan <amyldunnigan@gmail.com>
2023-12-01 18:01:53 +0000

    Basic game

--- main2.cpp
+++ main2.cpp
@@ -1,358 +1,156 @@
-/*******************************************************************************************
-*
-*   raylib - classic game: asteroids survival
-*
-*   Sample game developed by Ian Eito, Albert Martos and Ramon Santamaria
-*
-*   This game has been created using raylib v1.3 (www.raylib.com)
-*   raylib is licensed under an unmodified zlib/libpng license (View raylib.h for details)
-*
-*   Copyright (c) 2015 Ramon Santamaria (@raysan5)
-*
-********************************************************************************************/
-
 #include "raylib.h"
-#include <math.h>
 
-#if defined(PLATFORM_WEB)
-    #include <emscripten/emscripten.h>
-#endif
-
-//----------------------------------------------------------------------------------
-// Some Defines
-//----------------------------------------------------------------------------------
-#define PLAYER_BASE_SIZE    20.0f
-#define PLAYER_SPEED        6.0f
-#define PLAYER_MAX_SHOOTS   10
-
-#define METEORS_SPEED       2
-#define MAX_MEDIUM_METEORS  8
-#define MAX_SMALL_METEORS   16
-
-#define MAX_FRAME_SPEED     15
-#define MIN_FRAME_SPEED      1
-
-//----------------------------------------------------------------------------------
-// Types and Structures Definition
-//----------------------------------------------------------------------------------
-typedef struct Player {
-    Vector2 position;
-    float speed;
-    float acceleration;
-    float rotation;
-    Vector3 collider;
-    Color color;
-    Texture2D texture;
-    Rectangle sourceRec;
-} Player;
-
-typedef struct Meteor {
-    Vector2 position;
-    Vector2 speed;
-    float radius;
-    bool active;
-    Color color;
-} Meteor;
-
-//------------------------------------------------------------------------------------
-// Global Variables Declaration
-//------------------------------------------------------------------------------------
-static const int screenWidth = 800;
-static const int screenHeight = 450;
-
-static bool gameOver = false;
-static bool pause = false;
-static Player player = { 0 };
-int currentFrame = 0;
-
-int framesCounter = 0;
-int framesSpeed = 8; 
-
-static Meteor mediumMeteor[MAX_MEDIUM_METEORS] = { 0 };
-static Meteor smallMeteor[MAX_SMALL_METEORS] = { 0 };
-
-//------------------------------------------------------------------------------------
-// Module Functions Declaration (local)
-//------------------------------------------------------------------------------------
-static void InitGame(void);         // Initialize game
-static void UpdateGame(void);       // Update game (one frame)
-static void DrawGame(void);         // Draw game (one frame)
-static void UnloadGame(void);       // Unload game
-static void UpdateDrawFrame(void);  // Update and Draw (one frame)
-
-
-
-//------------------------------------------------------------------------------------
-// Program main entry point
-//------------------------------------------------------------------------------------
-int main(void)
+struct Anim
 {
-    // Initialization (Note windowTitle is unused on Android)
-    //---------------------------------------------------------
-    InitWindow(screenWidth, screenHeight, "classic game: asteroids survival");
+    Rectangle rec;
+    Vector2 pos;
+    int frame;
+    float updateTime;
+    float runningTime;
+};
+
+int main()
+{
+    const int screenWidth = 800;
+    const int screenHeight = 600;
     
-    struct Player player;
-    player.position = (Vector2){screenWidth/2, screenHeight/2};
-    player.speed = 5.0f;
-    player.texture = LoadTexture("Resources/Textures/scarfy.png");
-    player.sourceRec = (Rectangle){0.0f, 0.0f, (float)(player.texture.width)/6, (float)(player.texture.height)};
-  
-    InitGame();
+    InitWindow(screenWidth, screenHeight, "First Game");
 
-#if defined(PLATFORM_WEB)
-    emscripten_set_main_loop(UpdateDrawFrame, 60, 1);
-#else
+    const int gravity{1000};
+    const int scarfyPadding{50};
 
+    Texture2D obstacle = LoadTexture("Resources/Textures/baddy.png");
+    Texture2D scarfy = LoadTexture("Resources/Textures/scarfy.png");
+    
+    Anim scarfyAnim;
+    scarfyAnim.rec.width = scarfy.width/6;
+    scarfyAnim.rec.height = scarfy.height;
+    scarfyAnim.rec.x = 0;
+    scarfyAnim.rec.y = 0;
+    scarfyAnim.pos.x = scarfyPadding;
+    scarfyAnim.pos.y = screenHeight - scarfyAnim.rec.height;
+    scarfyAnim.frame = 0;
+    scarfyAnim.updateTime = 1.0/12.0;
+    scarfyAnim.runningTime = 0.0;
+
+    Rectangle obRec;
+    obRec.width = obstacle.width;
+    obRec.height = obstacle.height;
+    obRec.x = 300;
+    obRec.y = 0;
+    Vector2 obPos;
+    obPos.x = screenWidth - obRec.width;
+    obPos.y = screenHeight - obRec.height;
+
+    int obVel{-100};
+
+    int velocity{0};
+    const int jumpHeight{500};
+    bool jumped{false};
+    int speed{200};
+    bool collision{};
+    
     SetTargetFPS(60);
-    //--------------------------------------------------------------------------------------
-
+    
     // Main game loop
     while (!WindowShouldClose())    // Detect window close button or ESC key
+    { const float deltaTime{GetFrameTime()};
+
+    Rectangle obstacleRec{
+        obPos.x,
+        obPos.y,
+        obRec.height,
+        obRec.width
+    };
+
+    Rectangle scarfyRec{
+        scarfyAnim.pos.x,
+        scarfyAnim.pos.y,
+        scarfyAnim.rec.height,
+        scarfyAnim.rec.width
+    };
+
+    if(CheckCollisionRecs(scarfyRec,obstacleRec))
     {
-
-        if (IsKeyDown(KEY_RIGHT)) player.position.x += player.speed;
-        if (IsKeyDown(KEY_LEFT)) player.position.x -= player.speed;
-        if (IsKeyDown(KEY_UP)) player.position.x -= player.speed;
-        if (IsKeyDown(KEY_DOWN)) player.position.x += player.speed;
-        // Update and Draw
-        //----------------------------------------------------------------------------------
-        UpdateDrawFrame();
-        //----------------------------------------------------------------------------------
-    }
-#endif
-    // De-Initialization
-    //--------------------------------------------------------------------------------------
-    UnloadGame();         // Unload loaded data (textures, sounds, models...)
-
-    CloseWindow();        // Close window and OpenGL context
-    //--------------------------------------------------------------------------------------
-
-    return 0;
-}
-
-//------------------------------------------------------------------------------------
-// Module Functions Definitions (local)
-//------------------------------------------------------------------------------------
-
-// Initialize game variables
-void InitGame(void)
-{
-    int posx, posy;
-    int velx, vely;
-    bool correctRange = false;
-
-    pause = false;
-
-    // Initialization player
-    player.position = (Vector2){screenWidth/2, screenHeight/2};
-    player.acceleration = 0;
-    player.rotation = 0;
-    player.collider = (Vector3){player.position.x + sin(player.rotation*DEG2RAD)*(player.texture.height/2.5f), player.position.y - cos(player.rotation*DEG2RAD)*(player.texture.height/2.5f), 12};
-
-    for (int i = 0; i < MAX_MEDIUM_METEORS; i++)
-    {
-        posx = GetRandomValue(0, screenWidth);
-
-        while (!correctRange)
-        {
-            if (posx > screenWidth/2 - 150 && posx < screenWidth/2 + 150) posx = GetRandomValue(0, screenWidth);
-            else correctRange = true;
-        }
-
-        correctRange = false;
-
-        posy = GetRandomValue(0, screenHeight);
-
-        while (!correctRange)
-        {
-            if (posy > screenHeight/2 - 150 && posy < screenHeight/2 + 150)  posy = GetRandomValue(0, screenHeight);
-            else correctRange = true;
-        }
-
-        correctRange = false;
-        velx = GetRandomValue(-METEORS_SPEED, METEORS_SPEED);
-        vely = GetRandomValue(-METEORS_SPEED, METEORS_SPEED);
-
-        while (!correctRange)
-        {
-            if (velx == 0 && vely == 0)
-            {
-                velx = GetRandomValue(-METEORS_SPEED, METEORS_SPEED);
-                vely = GetRandomValue(-METEORS_SPEED, METEORS_SPEED);
-            }
-            else correctRange = true;
-        }
-        mediumMeteor[i].position = (Vector2){posx, posy};
-        mediumMeteor[i].speed = (Vector2){velx, vely};
-        mediumMeteor[i].radius = 20;
-        mediumMeteor[i].active = true;
-        mediumMeteor[i].color = GREEN;
+        collision = true;     
     }
 
-    for (int i = 0; i < MAX_SMALL_METEORS; i++)
-    {
-        posx = GetRandomValue(0, screenWidth);
+    if(IsKeyDown(KEY_RIGHT) && !jumped){
+        scarfyAnim.pos.x += speed* deltaTime;
+        scarfyAnim.rec.width = scarfy.width/6;
 
-        while (!correctRange)
-        {
-            if (posx > screenWidth/2 - 150 && posx < screenWidth/2 + 150) posx = GetRandomValue(0, screenWidth);
-            else correctRange = true;
-        }
-
-        correctRange = false;
-
-        posy = GetRandomValue(0, screenHeight);
-
-        while (!correctRange)
-        {
-            if (posy > screenHeight/2 - 150 && posy < screenHeight/2 + 150)  posy = GetRandomValue(0, screenHeight);
-            else correctRange = true;
-        }
-
-        correctRange = false;
-        velx = GetRandomValue(-METEORS_SPEED, METEORS_SPEED);
-        vely = GetRandomValue(-METEORS_SPEED, METEORS_SPEED);
-
-        while (!correctRange)
-        {
-            if (velx == 0 && vely == 0)
-            {
-                velx = GetRandomValue(-METEORS_SPEED, METEORS_SPEED);
-                vely = GetRandomValue(-METEORS_SPEED, METEORS_SPEED);
+        scarfyAnim.runningTime += deltaTime;
+        if(scarfyAnim.runningTime>= scarfyAnim.updateTime){
+            scarfyAnim.runningTime = 0.0;
+            scarfyAnim.rec.x = scarfyAnim.frame * scarfyAnim.rec.width;
+            scarfyAnim.frame ++;
+            if (scarfyAnim.frame>5){
+                scarfyAnim.frame = 0;
             }
-            else correctRange = true;
         }
-        smallMeteor[i].position = (Vector2){posx, posy};
-        smallMeteor[i].speed = (Vector2){velx, vely};
-        smallMeteor[i].radius = 10;
-        smallMeteor[i].active = true;
-        smallMeteor[i].color = YELLOW;
+
     }
-}
 
-// Update game (one frame)
-void UpdateGame(void)
-{
-    if (!gameOver)
-    {
-        if (IsKeyPressed('P')) pause = !pause;
+    if(IsKeyReleased(KEY_RIGHT) && !jumped){
+        scarfyAnim.frame = 0;
+        scarfyAnim.rec.x = scarfyAnim.frame * scarfyAnim.rec.width;
+       
+    }
 
-        if (!pause)
-        {
-            framesCounter++;
+        if(IsKeyDown(KEY_LEFT) && !jumped){
+        scarfyAnim.pos.x -= speed* deltaTime;
+        scarfyAnim.rec.width = -scarfy.width/6;
 
-            // Player logic
-                   
-            if (framesSpeed > MAX_FRAME_SPEED) framesSpeed = MAX_FRAME_SPEED;
-            else if (framesSpeed < MIN_FRAME_SPEED) framesSpeed = MIN_FRAME_SPEED;
-
-            // Collision Player to meteors
-            player.collider = (Vector3){player.position.x + sin(player.rotation*DEG2RAD)*(player.texture.height/2.5f), player.position.y - cos(player.rotation*DEG2RAD)*(player.texture.height/2.5f), 12};
-
-            for (int a = 0; a < MAX_MEDIUM_METEORS; a++)
-            {
-                if (CheckCollisionCircles((Vector2){player.collider.x, player.collider.y}, player.collider.z, mediumMeteor[a].position, mediumMeteor[a].radius) && mediumMeteor[a].active) gameOver = true;
-            }
-
-            for (int a = 0; a < MAX_SMALL_METEORS; a++)
-            {
-                if (CheckCollisionCircles((Vector2){player.collider.x, player.collider.y}, player.collider.z, smallMeteor[a].position, smallMeteor[a].radius) && smallMeteor[a].active) gameOver = true;
-            }
-
-            // Meteor logic
-
-            for (int i = 0; i < MAX_MEDIUM_METEORS; i++)
-            {
-                if (mediumMeteor[i].active)
-                {
-                    // movement
-                    mediumMeteor[i].position.x += mediumMeteor[i].speed.x;
-                    mediumMeteor[i].position.y += mediumMeteor[i].speed.y;
-
-                    // wall behaviour
-                    if  (mediumMeteor[i].position.x > screenWidth + mediumMeteor[i].radius) mediumMeteor[i].position.x = -(mediumMeteor[i].radius);
-                    else if (mediumMeteor[i].position.x < 0 - mediumMeteor[i].radius) mediumMeteor[i].position.x = screenWidth + mediumMeteor[i].radius;
-                    if (mediumMeteor[i].position.y > screenHeight + mediumMeteor[i].radius) mediumMeteor[i].position.y = -(mediumMeteor[i].radius);
-                    else if (mediumMeteor[i].position.y < 0 - mediumMeteor[i].radius) mediumMeteor[i].position.y = screenHeight + mediumMeteor[i].radius;
-                }
-            }
-
-            for (int i = 0; i < MAX_SMALL_METEORS; i++)
-            {
-                if (smallMeteor[i].active)
-                {
-                    // movement
-                    smallMeteor[i].position.x += smallMeteor[i].speed.x;
-                    smallMeteor[i].position.y += smallMeteor[i].speed.y;
-
-                    // wall behaviour
-                    if  (smallMeteor[i].position.x > screenWidth + smallMeteor[i].radius) smallMeteor[i].position.x = -(smallMeteor[i].radius);
-                    else if (smallMeteor[i].position.x < 0 - smallMeteor[i].radius) smallMeteor[i].position.x = screenWidth + smallMeteor[i].radius;
-                    if (smallMeteor[i].position.y > screenHeight + smallMeteor[i].radius) smallMeteor[i].position.y = -(smallMeteor[i].radius);
-                    else if (smallMeteor[i].position.y < 0 - smallMeteor[i].radius) smallMeteor[i].position.y = screenHeight + smallMeteor[i].radius;
-                }
+        scarfyAnim.runningTime += deltaTime;
+        if(scarfyAnim.runningTime>= scarfyAnim.updateTime){
+            scarfyAnim.runningTime = 0.0;
+            scarfyAnim.rec.x = scarfyAnim.frame * scarfyAnim.rec.width;
+            scarfyAnim.frame ++;
+            if (scarfyAnim.frame>5){
+                scarfyAnim.frame = 0;
             }
         }
     }
+
+    if(IsKeyReleased(KEY_LEFT) && !jumped){
+        scarfyAnim.frame = 0;
+        scarfyAnim.rec.x = scarfyAnim.frame * scarfyAnim.rec.width;       
+    }
+
+    if(scarfyAnim.pos.y >= screenHeight - scarfy.height)
+        {
+            velocity = 0;
+            jumped = false;
+        }
     else
-    {
-        if (IsKeyPressed(KEY_ENTER))
         {
-            InitGame();
-            gameOver = false;
+            velocity += gravity * deltaTime;
+            jumped = true;
         }
+
+    if (IsKeyPressed(KEY_SPACE) && !jumped)
+        {
+            velocity -= jumpHeight; 
+        }
+
+        scarfyAnim.pos.y += velocity * deltaTime;
+
+        obPos.x += obVel * deltaTime;
+
+    if (!collision)
+        {
+            DrawTextureRec(scarfy, scarfyAnim.rec, scarfyAnim.pos, WHITE);
+            DrawTextureRec(obstacle, obRec, obPos, WHITE);
+        }
+
+            BeginDrawing();
+            ClearBackground(RAYWHITE);
+            EndDrawing();        
+                
     }
-}
-
-// Draw game (one frame)
-void DrawGame(void)
-{
-    BeginDrawing();
-
-        ClearBackground(LIGHTGRAY);
-        
-        DrawTextureRec(player.texture, player.sourceRec, player.position, WHITE);   
-
-        if (!gameOver)
-        {
-            // Draw player
-        
-        DrawTextureRec(player.texture, player.sourceRec, player.position, WHITE);   
-           
-            // Draw meteor
-            for (int i = 0;i < MAX_MEDIUM_METEORS; i++)
-            {
-                if (mediumMeteor[i].active) DrawCircleV(mediumMeteor[i].position, mediumMeteor[i].radius, GRAY);
-                else DrawCircleV(mediumMeteor[i].position, mediumMeteor[i].radius, Fade(LIGHTGRAY, 0.3f));
-            }
-
-            for (int i = 0;i < MAX_SMALL_METEORS; i++)
-            {
-                if (smallMeteor[i].active) DrawCircleV(smallMeteor[i].position, smallMeteor[i].radius, DARKGRAY);
-                else DrawCircleV(smallMeteor[i].position, smallMeteor[i].radius, Fade(LIGHTGRAY, 0.3f));
-            }
+    UnloadTexture(scarfy);    
+    UnloadTexture(obstacle);
+    CloseWindow();       
+    return 0;       
             
-            DrawTextureRec(player.texture, player.sourceRec, player.position, WHITE);   
-            DrawText(TextFormat("TIME: %.02f", (float)framesCounter/60), 10, 10, 20, BLACK);
-
-            if (pause) DrawText("GAME PAUSED", screenWidth/2 - MeasureText("GAME PAUSED", 40)/2, screenHeight/2 - 40, 40, GRAY);
-        }
-        else DrawText("PRESS [ENTER] TO PLAY AGAIN", GetScreenWidth()/2 - MeasureText("PRESS [ENTER] TO PLAY AGAIN", 20)/2, GetScreenHeight()/2 - 50, 20, GRAY);
-
-    EndDrawing();
-    //----------------------------------------------------------------------------------
-}
-
-// Unload game variables
-void UnloadGame(void)
-{
-    // TODO: Unload all dynamic loaded data (textures, sounds, models...)
-}
-
-// Update and Draw (one frame)
-void UpdateDrawFrame(void)
-{
-    UnloadTexture(player.texture);
-    UpdateGame();
-    DrawGame();
 }
